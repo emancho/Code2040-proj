67bb5fc99494be246d7c3ad92553ced22a3e037b
Sean Brown <seanbrhn3@gmail.com>
2018-07-21 11:18:05 -0700

    commiting change

--- web_app/app.py
+++ web_app/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 links_to_code = ['https://www.codecademy.com/','https://www.codewars.com/dashboard','https://codefights.com/']
 housing_links = ['https://larkinstreetyouth.org/','http://lyric.org/','http://www.homelessyouthalliance.org/','http://www.firstplaceforyouth.org/','https://atthecrossroads.org/','https://www.ccsf.edu/en/student-services/student-counseling/guardians-scholars-program.html']
+
 @app.route('/')
 def index():
     return render_template('index.html')
