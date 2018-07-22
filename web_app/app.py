80b38b8c29d631d61d7f4a71f9c30dde5b330dcd
Sean Brown <seanbrhn3@gmail.com>
2018-07-21 17:04:52 -0700

    mvp done

--- web_app/app.py
+++ web_app/app.py
@@ -5,7 +5,7 @@ links_to_code = ['https://www.codecademy.com/','https://www.codewars.com/dashboa
 housing_links = ['https://larkinstreetyouth.org/','http://lyric.org/','http://www.homelessyouthalliance.org/','http://www.firstplaceforyouth.org/','https://atthecrossroads.org/','https://www.ccsf.edu/en/student-services/student-counseling/guardians-scholars-program.html']
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('coding.html',links_to_code=links_to_code)
 
 @app.route('/coding')
 def coding():
---
+++ web_app/jobs/jobs/spiders/job_spider.py
@@ -0,0 +1,8 @@
+# import scrapy
+#
+# class JobSpider(scrapy.Spider):
+#     name="jobs"
+#     starturls = ['https://www.indeed.com/q-Shelter-l-San-Francisco,-CA-jobs.html']
+#
+#     def parse(self,response):
+#
